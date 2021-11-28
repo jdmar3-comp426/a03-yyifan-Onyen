947e9e5bbbc9b0efd43c0388cdf355696527aa0d
yyifan-Onyen <yyifan@ad.unc.edu>
2021-11-28 11:46:35 -0500

    test2

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -167,7 +167,7 @@ export const someEven = (arr, test) => {
  *       -->  { pass: [1, 5, 31], fail: [90] }
  */
 export const filter = (arr, test) => {
-    const all= {fail: [], pass: []};
+    const all= {pass:[], fail:[]};
     for(let i =0; i<arr.length; i++){
         if(test(arr[i])){
             all.pass.push(arr[i]);
