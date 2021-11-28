210066a74241ded2d8a926ff9c0bc7310dccdd59
yyifan-Onyen <yyifan@ad.unc.edu>
2021-11-27 23:40:04 -0500

    test2

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -72,7 +72,10 @@ export const tenTimes = undefined;
  *   function to multiply 50 by 10 and returns the result.
  */
 export const tenTimesFifty = () => {
-
+tenTimes = (num) =>{
+    return multiplyBy(num)(10);
+}
+return tenTimes(50);
 };
 
 
