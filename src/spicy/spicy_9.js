83ad11d406fb9e8a569b3251d5522e5047b56b9c
yyifan-Onyen <yyifan@ad.unc.edu>
2021-11-28 11:24:34 -0500

    test2

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -142,7 +142,6 @@ export const someEven = (arr, test) => {
         }
     }
     return false;
-
 };
 
 
@@ -168,7 +167,17 @@ export const someEven = (arr, test) => {
  *       -->  { pass: [1, 5, 31], fail: [90] }
  */
 export const filter = (arr, test) => {
-
+    const pass=[];
+    const fail=[];
+    for(let i =0; i<arr.length; i++){
+        if(test[arr[i]]){
+            pass.push(arr[i]);
+        }
+        else{
+            fail.push(arr[i]);
+        }
+    }
+    return [pass,fail];
 };
 
 
