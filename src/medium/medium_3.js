7294791cf3d0345e7b018a9b9379a4e910619f24
yyifan-Onyen <yyifan@ad.unc.edu>
2021-12-01 13:24:54 -0500

    test2

--- src/medium/medium_3.js
+++ src/medium/medium_3.js
@@ -18,6 +18,16 @@ queries.
  *
  */
 export function searchHighPower(car_data, minHorsepower, minTorque) {
+    let combine=[];
+    for(let i =0; i< car_data.length; i++){
+        if(car_data[i].horsepower>= minHorsepower && car_data[i].minTorque>=minTorque){
+            combine.push(car_data[i]);
+        }
+    }
+    combine.sort(function(a, b){
+        return a.horsepower-b.horsepower;
+    });
+    return combine;
 
 }
 
